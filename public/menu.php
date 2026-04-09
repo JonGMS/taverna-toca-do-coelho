6a100e9f7ae4d18e4f57e80ce55025f72709e337
JonGMS <joaoggabriell@gmail.com>
2026-04-08 22:28:35 -0300

    feat: Criação do Menu: Padronização de tela

---
+++ public/menu.php
@@ -0,0 +1,21 @@
+<div class="header_menu">
+    <div class="header_menu__buttons">
+        <div class="menu_left">
+            <div class="button"><a href="">COMANDA</a></div>
+            <div class="button"><a href="">CARDÁPIO</a></div>
+            <div class="button"><a href="">DRINKS</a></div>
+            <div class="button"><a href="">JOGOS</a></div>
+        </div>
+
+        <div class="menu_icon">
+            <img src="assets/img/Logo.png" alt="">
+        </div>
+
+        <div class="menu_right">
+            <div class="button"><a href="">INGREDIENTE</a></div>
+            <div class="button"><a href="">CLIENTE</a></div>
+            <div class="button"><a href="">FUNCIONÁRIO</a></div>
+        </div>
+    </div>
+
+</div>
