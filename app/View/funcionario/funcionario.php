74148734b673a05724180c01e073d70bd48b3ed7
JonGMS <joaoggabriell@gmail.com>
2026-04-10 21:02:42 -0300

    feat: Tela funcionário

---
+++ app/View/funcionario/funcionario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/assets/css/pages/menu.css">
+    <title>Funcionário</title>
+</head>
+<body>
+    <header>
+        <?php include "../layouts/menu.php" ?>
+    </header>
+
+</body>
+</html>
